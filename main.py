7ffb04d1f9243c4a48d954b23deb7632dd2711ce
artemresty07-byte <artemresty07@gmail.com>
2026-04-01 23:38:47 +0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import discord
+import os
+from flask import Flask
+from threading import Thread
+
+# Создаем мини-сервер, чтобы Render не отключал бота
+app = Flask('')
+@app.route('/')
+def home():
+    return "I'm alive"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+class MyBot(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
+        await self.change_presence(activity=discord.Game(name="Build & Scripts | Shop"))
+
+# Запуск
+keep_alive()
+token = os.environ.get("DISCORD_TOKEN")
+client = MyBot(intents=discord.Intents.default())
+client.run(token)
 
